0e88a01e97ad3ca915295471966db54d3c086430
anon <109340393+a-n0n-s3rv14m@users.noreply.github.com>
2022-12-06 08:10:45 +0100

    Test1

--- src/com/atos/Main.java
+++ src/com/atos/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
             String input;
             do {
-                System.out.print("Zugfolge:");
+                System.out.print("Zugfolg\ne:");
                 input = scanner.nextLine();
                 von = input.substring(0,2);
                 zu = input.substring(3,5);
