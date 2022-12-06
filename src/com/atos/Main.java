799820fcb737083d8e18a77bca3c944ccde857de
anon <109340393+a-n0n-s3rv14m@users.noreply.github.com>
2022-12-06 12:00:47 +0100

    Erste Startfunktionen

--- src/com/atos/Main.java
+++ src/com/atos/Main.java
@@ -2,26 +2,53 @@ package com.atos;
 
 import com.atos.exceptions.InvalidMoveException;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
         String von;
         String zu;
-        String defaultFenString = "1p1p1p1p/p1p1p1p1/1p1p1p1p/8/8/P1P1P1P1/1P1P1P1P/P1P1P1P1 w";
+        String defaultFenString = "1p1p1p1p/p1p1p1p1/1p1p1p1p/8/8/P1P1P1P1/1P1P1P1P/P1P1P1P1 w"; //Small Char is Black
 
         try {
             Game game = new Game();
 
             String input;
+            String configWahl;
             do {
-                System.out.print("Zugfolge\ne:");
+
+
+                System.out.print("Hallo wählen sie eine Startkonfiguration\n" +
+                        "(1)Standard\n" +
+                        "(2)Eigene\n" +
+                        "Eingabe:");
+
+                configWahl = scanner.nextLine();
+
+                if (configWahl.equals("1")) {
+
+                } else if (configWahl.equals("2")) {
+                    System.out.print("Bitte gebe deine Startposition in FEN an\n" +
+                            "Eingabe(FEN):");
+                    String fen = scanner.nextLine();
+                    defaultFenString = fen;
+                }
+
+                System.out.println("Aktueller Spielstand = "+ defaultFenString);
+
+                System.out.println("Gib deinen Zug an:");
                 input = scanner.nextLine();
+
                 von = input.substring(0,2);
                 zu = input.substring(3,5);
                 System.out.println(von);
-                System.out.println(zu);
+                System.out.println(zu); //asdasdas
 
             } while (!input.equals(""));
 
