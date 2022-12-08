3043f5a609cb82d6e0696fd638ef7a5df158a5e4
anon <109340393+a-n0n-s3rv14m@users.noreply.github.com>
2022-12-08 15:13:54 +0100

    Pieces schlagen + Pieces bwegen +

--- src/com/atos/Game.java
+++ src/com/atos/Game.java
@@ -10,6 +10,8 @@ import java.util.Observable;
 
 public class Game extends Observable {
 
+
+    public boolean ismoved;
     protected int d1x, d1y, d2x, d2y;
     protected Board board;
 
@@ -85,6 +87,130 @@ public class Game extends Observable {
         this.currentPlayer = this.currentPlayer.getColor() == this.players[0].getColor() ? this.players[1] : this.players[0];
     }
 
+    public void advance(Field from, Field to) throws InvalidMoveException, InvalidPlayerException, NoMoveException{
+
+        boolean isMoveDiagonal = Math.abs(from.getX() - to.getX()) == Math.abs(from.getY() - to.getY());
+
+        boolean isMoveAllowed = true;
+        boolean twoPiecesbeside = true;
+
+        if (from.isEmpty() || !to.isEmpty() || !isMoveDiagonal) {
+
+            throw new InvalidMoveException();
+
+        }
+
+        if (from.getGamePiece().getColor() != this.currentPlayer.getColor()) {
+            throw new InvalidMoveException();
+        }
+
+        if (from.equals(to)) {
+            throw new NoMoveException();
+        }
+
+        int xFrom = from.getX();
+        int yFrom = from.getY();
+
+        int xTo = to.getX();
+        int yTo = to.getY();
+
+        ArrayList<GamePiece> ueberflogenePieces = new ArrayList<>();
+        int ueberflogeneLeereFields = 0;
+
+        GamePiece RelevantPieceForColorCheck = null;
+        GamePiece PieceFrom = from.getGamePiece();
+
+        boolean zugNachLinksUnten = xTo - xFrom < 0 && yTo - yFrom > 0;
+        boolean zugNachRechtsUnten = xTo - xFrom > 0 && yTo - yFrom > 0;
+        boolean zugNachRechtsOben = xTo - xFrom > 0 && yTo - yFrom < 0;
+        boolean zugNachLinksOben = xTo - xFrom < 0 && yTo - yFrom < 0;
+
+        for (int i = 1 ; i <= Math.abs(yFrom-yTo); i++) {
+
+
+            if (zugNachLinksOben) {
+                //Zug nach Links oben
+                RelevantPieceForColorCheck = board.getField(from.getX() - i, from.getY() - i).getGamePiece();
+
+            } else if (zugNachRechtsOben) {
+                //Zug nach Rechts oben
+                RelevantPieceForColorCheck = board.getField(from.getX() + i, from.getY() - i).getGamePiece();
+
+
+            } else if (zugNachRechtsUnten) {
+                //Zug nach Rechts unten
+                RelevantPieceForColorCheck = board.getField(from.getX() + i, from.getY() + i).getGamePiece();
+
+
+            } else if (zugNachLinksUnten) {
+                //Zug nach Links unten
+                RelevantPieceForColorCheck = board.getField(from.getX() - i, from.getY() + i).getGamePiece();
+
+            }
+
+            if (getCurrentPlayer().getColor() == CheckersColor.WHITE) {
+                if (zugNachRechtsUnten || zugNachLinksUnten) {
+                    isMoveAllowed = false;
+                }
+            }
+            if ( getCurrentPlayer().getColor() == CheckersColor.BLACK) {
+                if (zugNachLinksOben || zugNachRechtsOben) {
+                    isMoveAllowed = false;
+                }
+            }
+
+
+            if (RelevantPieceForColorCheck != null && RelevantPieceForColorCheck.getColor() != PieceFrom.getColor()) {
+                ueberflogenePieces.add(RelevantPieceForColorCheck);
+                ueberflogeneLeereFields = 0;
+            }
+            if (RelevantPieceForColorCheck == null) {
+                ueberflogenePieces.clear();
+                ueberflogeneLeereFields += 1;
+            }
+
+            if (ueberflogenePieces.toArray().length > 1) {
+                isMoveAllowed = false;
+            }
+
+            if (ueberflogeneLeereFields > 1) {
+                isMoveAllowed = false;
+            }
+
+            if (RelevantPieceForColorCheck != null && RelevantPieceForColorCheck.getColor() == PieceFrom.getColor()) {
+                isMoveAllowed = false;
+            }
+        }
+
+        if (isMoveAllowed) {
+
+            Move m = new Move(this.currentPlayer,from, to);
+
+            this.moves.add(m);
+
+            killGamePiece(this.getSelection(), to);
+
+            to.setGamePiece(from.getGamePiece());
+            from.setGamePiece(null);
+
+            // TODO check if game piece is captured
+            ismoved = true;
+
+            // switch player
+            this.currentPlayer = this.currentPlayer.getColor() == this.players[0].getColor() ? this.players[1] : this.players[0];
+
+
+
+
+            setSelection(null);
+
+        } else {
+            throw new InvalidMoveException();
+
+        }
+
+    }
+
 
 
     public String toFenString() {
@@ -116,50 +242,134 @@ public class Game extends Observable {
 
     public void setOptionsSelection() {
 
+        boolean goDown = false;
+
         int x = this.selection.getX();
         int y = this.selection.getY();
 
         System.out.println(this.selection.getColor());
+        for (int i = 1; i < 3; i++) {
 
-        if (currentPlayer.getColor() == CheckersColor.WHITE && selection.getGamePiece().getColor() == CheckersColor.WHITE) {
-            d1x = x-1;
-            d1y = y-1;
+            if (currentPlayer.getColor() == CheckersColor.WHITE && selection.getGamePiece().getColor() == CheckersColor.WHITE) {
 
-            d2x = x+1;
-            d2y = y-1;
+                goDown = false;
 
-        }
-        else if (currentPlayer.getColor() == CheckersColor.BLACK && selection.getGamePiece().getColor() == CheckersColor.BLACK) {
-            d1x = x-1;
-            d1y = y+1;
+                d1x = x-i;
+                d1y = y-i;
 
-            d2x = x-1;
-            d2y = y+1;
+                d2x = x+i;
+                d2y = y-i;
 
-        }
-        if (d1x < 8 && d1x >= 0 && d1y >=0 && d1y < 8) {
-            if (this.board.getField(d1x, d1y).getGamePiece()  == null) {
-                this.optionsSelection.add(this.board.getField(d1x, d1y));
             }
-        }
-        if (d2x < 8 && d2x >= 0 && d2y >=0 && d2y < 8) {
-            if (this.board.getField(d2x, d2y).getGamePiece() == null) {
+            else if (currentPlayer.getColor() == CheckersColor.BLACK && selection.getGamePiece().getColor() == CheckersColor.BLACK) {
 
-                this.optionsSelection.add(this.board.getField(d2x, d2y));
+                goDown = true;
+
+                d1x = x-i;
+                d1y = y+i;
+
+                d2x = x+i;
+                d2y = y+i;
+
+            }
+            if (this.optionsSelection.toArray().length > 1) {
+                break;
+            }
+            if (d1x < 8 && d1x >= 0 && d1y >=0 && d1y < 8) {
+                if (this.board.getField(d1x, d1y).getGamePiece()  == null) {
+
+                    this.optionsSelection.add(this.board.getField(d1x, d1y));
+                }
+            }
+            if (d2x < 8 && d2x >= 0 && d2y >=0 && d2y < 8) {
+                if (this.board.getField(d2x, d2y).getGamePiece() == null) {
+
+                    this.optionsSelection.add(this.board.getField(d2x, d2y));
+                }
             }
         }
     }
 
     public void setSelection(Field selection) {
-        while(this.optionsSelection.size() > 0) {
-            optionsSelection.remove(0);
-        }
+        optionsSelection.clear();
 
         this.selection = selection;
+        if ( selection != null) {
 
-        this.setOptionsSelection();
+            this.setOptionsSelection();
+
+        }
 
         setChanged();
         notifyObservers();
+    }
+
+    public void killGamePiece ( Field from, Field to) throws InvalidMoveException {
+        int xFrom = from.getX();
+        int yFrom = from.getY();
+
+        int xTo = to.getX();
+        int yTo = to.getY();
+
+
+
+        boolean gradeGeschmissen = false;
+        GamePiece IteratedGamePieceToCheck = null;
+        Field FieldinQuestion = null;
+        Field folgendesFieldinQuestion = null;
+        GamePiece SelectedGamePieceFrom = this.getSelection().getGamePiece();
+        GamePiece GamePieceTo = to.getGamePiece();
+
+        for (int i = 0; i <= Math.abs(yTo-yFrom); i++) {
+
+
+            if (xTo - xFrom < 0 && yTo - yFrom < 0) {
+                //Zug nach Links oben
+                FieldinQuestion = this.getBoard().getField(xFrom - i, yFrom - i);
+
+
+                IteratedGamePieceToCheck = this.getBoard().getField(xFrom - i, yFrom - i).getGamePiece();
+
+
+            } else if (xTo - xFrom > 0 && yTo - yFrom < 0) {
+                //Zug nach Rechts oben
+                FieldinQuestion = this.getBoard().getField(xFrom + i, yFrom - i);
+
+
+                IteratedGamePieceToCheck = this.getBoard().getField(xFrom + i, yFrom - i).getGamePiece();
+
+
+            } else if (xTo - xFrom > 0 && yTo - yFrom > 0) {
+                //Zug nach Rechts unten
+                FieldinQuestion = this.getBoard().getField(xFrom + i, yFrom + i);
+
+
+                IteratedGamePieceToCheck = this.getBoard().getField(xFrom + i, yFrom + i).getGamePiece();
+
+
+            } else if (xTo - xFrom < 0 && yTo - yFrom > 0) {
+                //Zug nach Links unten
+                FieldinQuestion = this.getBoard().getField(xFrom - i, yFrom + i);
+
+
+                IteratedGamePieceToCheck = this.getBoard().getField(xFrom - i, yFrom + i).getGamePiece();
+
+
+            }
+
+
+            if (!FieldinQuestion.isEmpty() && IteratedGamePieceToCheck != SelectedGamePieceFrom && IteratedGamePieceToCheck != GamePieceTo ) {
+
+
+                    FieldinQuestion.setGamePiece(null);
+                    System.out.println("GESCHMISSEN!!");
+                    gradeGeschmissen = true;
+
+
+
+            } else {
+                gradeGeschmissen = false;
+            }
+        }
     }
 }
--- src/com/atos/components/FieldStack.java
+++ src/com/atos/components/FieldStack.java
@@ -2,6 +2,10 @@ package com.atos.components;
 
 import com.atos.Field;
 import com.atos.Game;
+import com.atos.GamePiece;
+import com.atos.exceptions.InvalidMoveException;
+import com.atos.exceptions.InvalidPlayerException;
+import com.atos.exceptions.NoMoveException;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -12,6 +16,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class FieldStack extends StackPane implements Observer {
+
+    int xFrom,yFrom,xTo,yTo;
     private Rectangle rectangle;
     private Text text;
     private Color color;
@@ -38,9 +44,26 @@ public class FieldStack extends StackPane implements Observer {
         setOnMouseClicked(event -> {
             // Hier muss alles programmiert werden was passieren soll wenn auf eine Figur geklickt wird.
 
-            if (!field.isEmpty()) {
-                Field von = field;
+            System.out.println(game.getBoard().toFenString());
 
+            if (field.isEmpty()) {
+
+                System.out.println(game.getSelection().getX()+ " " + game.getSelection().getY());
+
+                Field to = field;
+
+
+                try {
+
+                    game.advance(game.getSelection(), to);
+
+                } catch (InvalidMoveException e){
+                    throw new RuntimeException(e);
+                } catch (InvalidPlayerException e) {
+                    throw new RuntimeException(e);
+                } catch (NoMoveException e) {
+                    throw new RuntimeException(e);
+                }
 
             }
 
@@ -82,6 +105,21 @@ public class FieldStack extends StackPane implements Observer {
                     rectangle.setFill(Color.ORANGE);
                 }
             }
+
+            text.setText(field.isEmpty() ? "" : String.valueOf(field.getGamePiece().getUnicode()));
+
+            if (game.ismoved == true) {
+                if (!field.equals(game.getSelection())) {
+                    game.ismoved = false;
+                    rectangle.setFill(Color.LIGHTGRAY);
+                }
+                for (int i = 0; i < game.getOptionsSelection().toArray().length; i++) {
+                    if (field.equals(game.getOptionsSelection().get(i))) {
+                        rectangle.setFill(Color.LIGHTGRAY);
+                    }
+                }
+            }
+
 
         }
     }
